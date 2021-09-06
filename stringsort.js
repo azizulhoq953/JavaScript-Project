79a1c6340d4b9311e84d17ebc2276693ff5de6ff
azizul_hoq <azizulhoq4305@gamil.com>
2021-09-06 17:21:25 +0600

    tutlsp

---
+++ stringsort.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana","orange","Apple","Mango"];
+
+console.log(fruits);
+fruits.sort();
+ fruits.reverse();
+ const points = [40, 100, 1, 5, 25, 10];
+console.log(points.sort(function(a, b){return b - a}));
