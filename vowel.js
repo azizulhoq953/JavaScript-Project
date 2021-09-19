88484f958d83d96a991a249c32135235659859a3
azizul_hoq <azizulhoq4305@gamil.com>
2021-09-19 11:11:18 +0600

    sumttuls

--- vowel.js
+++ vowel.js
@@ -1,12 +1,17 @@
-var latter = prompt("Enter Any Latter In Check Vowel And Consonant")
+// var latter = prompt("Enter Any Latter In Check Vowel And Consonant")
 
-latter=latter.toLowerCase();
+// latter=latter.toLowerCase();
 
-if (latter=="a" || latter=="e" || latter=="i" || latter=="o" || latter=="u")
-document.write("Vowel")
+// if (latter=="a" || latter=="e" || latter=="i" || latter=="o" || latter=="u")
+// document.write("Vowel")
 
-/*else if (latter=="A" || latter=="E" || latter=="I" || latter=="O" || latter=="U")
-document.write("Vowel")*/
+// /*else if (latter=="A" || latter=="E" || latter=="I" || latter=="O" || latter=="U")
+// document.write("Vowel")*/
 
-else
-document.write("Consonant")
+// else
+// document.write("Consonant")
+const myGril = ["Cecilie","Lone"];
+const myBoys = ["Email","Tobies","Linus"];
+const myChild =myGril.concat(myBoys);
+
+document.write(myChild);
