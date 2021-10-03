c08c5dddd6f981de49c795904a38c17607c8314d
azizul_hoq <azizulhoq4305@gamil.com>
2021-10-03 11:52:01 +0600

    ans

---
+++ Date.js
@@ -0,0 +1,18 @@
+var date = new Date();
+console.log(date);
+
+var year = date.getFullYear();
+console.log(year);
+
+var day = date.getDay();
+console.log(day);
+
+var month = date.getMonth();
+console.log(month);
+
+var Update= date.getDate();
+console.log(Update);
+
+var Hour= date.getHours();
+console.log(Hour);
+
---
+++ access.js
@@ -0,0 +1,34 @@
+// var first= document.getElementById("my")
+// first.innerHTML = "My Name Is Azizul"+ " My Dream Creator Of ";
+
+// document.getElementById("hobby").innerHTML="Whole Country";
+
+
+//document.querySelector(".my").innerHTML="First Changed Query";
+
+// document.querySelector("a").innerHTML="Logo";
+// document.querySelector("li a").innerHTML="study";
+// document.querySelector(".my-div a").innerHTML="Details";
+
+//onclick proparty
+// function myMessage(){
+//  document.querySelector("#paraid").innerHTML="This is test";
+// }
+// var test = document.querySelector(".paraid2");
+// function para1(){
+//     var test = document.querySelector(".paraid2");
+//     test.innerHTML="Hey What Is Your Name.?";
+//     test.innerHTML="Hey Man How Are You.?";
+//}
+var myimg2 =  document.querySelectorsrc("#myimg1");
+function myPicture1(){
+
+    myimg2.src="img/az1.jpg";
+  
+}
+
+function myPicture2(){
+
+    myimg2.src="img/az2.jpg";
+  
+}
