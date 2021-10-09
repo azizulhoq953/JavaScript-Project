afd1df25ed756dc591a3dce08d13b2a2a2ac2986
azizul_hoq <azizulhoq4305@gamil.com>
2021-10-09 10:49:04 +0600

    sumiit

--- doc.js
+++ doc.js
@@ -1,6 +1,11 @@
-console.dir(document)
-console.dir(document.domain)
-console.dir(document.port)
-console.dir(document.URL)
-console.dir(document.port)
-console.dir(document.div)
+// console.dir(document)
+// console.dir(document.domain)
+// console.dir(document.port)
+// console.dir(document.URL)
+// console.dir(document.port)
+// console.dir(document.div)
+
+let element=document.getElementById('lname')
+
+console.log(element.textContent);
+console.log(element.innerText);
