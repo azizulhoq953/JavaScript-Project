c41d0d98238a39a0ed06dbb463e4843100706ec8
azizul_hoq <azizulhoq4305@gamil.com>
2021-10-19 10:10:11 +0600

    W3Practice

--- es6.js
+++ es6.js
@@ -1,4 +1,39 @@
+//es6 Features
+
 "use struct"
+
+//spreed opearator
+// function addNumbers(x,y,z){
+//     return x+y+z;
+// }
+// let numbers2 = [5,6,7]
+// //console.log(addNumbers(numbers[0],numbers[1],numbers[2]));
+// //console.log(addNumbers(...numbers));
+
+// // let numbers1=[4,5,6,9, ...numbers];
+// // console.log(numbers1)
+// let numbers3=[4,8,9]
+// let numbers=[...numbers2, ...numbers3] //concate using spreed opearator
+// console.log(numbers)
+
+//concatenate object using spreed opearator
+let p1={
+    name:'Azizul Hoq',
+    age:21,
+
+}
+
+let p2={
+    nationality:'Bangladesh',
+    occupation:'student',
+
+}
+let p={...p1, ...p2}
+console.log(p);
+
+
+
+
 
 // //Default Parameter
 // function message(Text="Hello Sir This is Default Parameter:"){
@@ -7,8 +42,8 @@
 // message()
 // message("I love Js ES6")
 
-//rest parameter 
-function sum(x,y, ...z){
-    console.log(`x=${x},y=${y} z=${z}`)
-}
-sum(10,20,30,40,66,70);
+// //rest parameter 
+// function sum(x,y, ...z){
+//     console.log(`x=${x},y=${y} z=${z}`)
+// }
+// sum(10,20,30,40,66,70);
