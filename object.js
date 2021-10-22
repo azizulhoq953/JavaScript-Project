9380fff7cdc5ec0eb21f98beb3f1f4854037cdb2
azizul_hoq <azizulhoq4305@gamil.com>
2021-10-22 16:11:58 +0600

    W3+ans

--- object.js
+++ object.js
@@ -1,10 +1,34 @@
+// const person= {
+//     firstname: 'azizul',
+//     lastName:'Hoq',
+//     Id:567089,
+//     fullName: function(){
+//         return this.firstname + " " + this.lastName+" "+ this.Id;
+//     }
+// }
+// console.log(person)
+// console.log(person.fullName())
+
+ //consize
+// const person= {
+//     firstname: 'azizul',
+//     lastName:'Hoq',
+//     Id:567089,
+//     fullName(){
+//         return this.firstname + " " + this.lastName+" "+ this.Id;
+//     }
+// }
+// console.log(person)
+// console.log(person.fullName())
+
+//consize 2nd  way in space use function
 const person= {
     firstname: 'azizul',
     lastName:'Hoq',
     Id:567089,
-    fullName: function(){
+    'full name'(){
         return this.firstname + " " + this.lastName+" "+ this.Id;
     }
 }
-console.log(person)
-console.log(person.fullName())
+//console.log(person)
+console.log(person['full name']())
