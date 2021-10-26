377efde5b8ddfa88f72a3ede06cff888280d4e36
azizul_hoq <azizulhoq4305@gamil.com>
2021-10-26 10:45:44 +0600

    anstutls

---
+++ forEach.js
@@ -0,0 +1,30 @@
+
+var numbers=[10,50,60,77];
+
+numbers.forEach(function(x,index,arr){
+arr[index]=x+5;
+
+})
+console.log(numbers);
+
+
+//squreNumber Using For Each
+// var numbers=[10,33,56,70];
+
+// var squreNumber=[];
+
+// numbers.forEach(function(x){
+//     squreNumber.push(x*x)
+// })
+// console.log(squreNumber);
+
+
+// var numbers=[10,30,50,66]
+
+
+
+// numbers.forEach(MyFunction)
+
+// function MyFunction(x){
+//     console.log(x)
+// }
