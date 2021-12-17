3f7fb63b228375379af63b35ed7d124f43f32f81
azizulhoq953 <azizulhoq2041@gmail.com>
2021-12-17 19:38:27 +0600

    API

--- Es6 Feature/API/4thApi.js
+++ Es6 Feature/API/4thApi.js
@@ -1,5 +1,6 @@
-// console.log(window);
 
+// console.log(window);
+//XML HTTP Request
 const request = new XMLHttpRequest();
 
 request.open("GET","https://jsonplaceholder.typicode.com/todos/1");
@@ -7,10 +8,32 @@ request.open("GET","https://jsonplaceholder.typicode.com/todos/1");
 request.send();
 request.onload=()=>{
     if(request.status === 200){
-        console.log(JSON.parse(request.response));
+        // console.log(JSON.parse(request.response));
     } else {
-        console.log(request);
-        console.log(`error ${request.status}`);
+        // console.log(request);
+        // console.log(`error ${request.status}`);
     }
 
 }
+
+//fetch api
+
+fetch('https://jsonplaceholder.typicode.com/todos/')
+
+.then(response=>{
+    // return response.json();
+}).then(json=>{
+    console.log(json);
+})
+
+//second Approach
+
+async function getUsers(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/')
+    let data = await response.json();
+    return data;
+}
+
+getUsers().then(response=>{
+    console.log(response)
+})
