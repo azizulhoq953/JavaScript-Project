910d940921020c2c1ad0ad7c352fc27d695ca68a
azizulhoq953 <azizulhoq2041@gmail.com>
2022-06-26 20:46:03 +0600

    self-update

---
+++ practice/june 25/forEach.js
@@ -0,0 +1,12 @@
+
+const numbers=[55,20,12,33,45];
+
+function myfunction(value,index,array){
+console.log(value)
+console.log(index)
+console.log(array)
+console.log("The End Array")
+
+}
+
+numbers.forEach(myfunction);
---
+++ practice/june 25/sceond.js
@@ -0,0 +1,13 @@
+document.getElementById("prt").innerHTML=5-6;
+// window.alert("Page Is Ready..")
+alert(54+10)
+document.getElementById("chng").innerText="Congratulations Is Modify"
+//let car=''
+//console.log(car);
+
+function myfunction (x,y){
+return x*y;
+}
+
+let m=myfunction(10,20)
+console.log(m);
