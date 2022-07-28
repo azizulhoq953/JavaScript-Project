b11585347ae5074776c072ab3a879f0c5ac160db
azizulhoq953 <azizulhoq2041@gmail.com>
2022-07-28 21:25:16 +0600

    tambuprtc

--- practice/june 25/sceond.js
+++ practice/june 25/sceond.js
@@ -1,14 +1,44 @@
-document.getElementById("prt").innerHTML=5-6;
-// window.alert("Page Is Ready..")
-alert(54+10)
-document.getElementById("chng").innerText="Congratulations Is Modify"
-//let car=''
-//console.log(car);
+// document.getElementById("prt").innerHTML=5-6;
+// // window.alert("Page Is Ready..")
+// alert(54+10)
+// document.getElementById("chng").innerText="Congratulations Is Modify"
+// //let car=''
+// //console.log(car);
 
-function myfunction (x,y){
-return x*y;
-}
+// function myfunction (x,y){
+//  return x+y;
+// }
 
-let m=myfunction(10,20)
-console.log(m);
-//function
+// let m=myfunction(10,20)
+// console.log(m);
+// //function
+/*
+//tabia study
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+
+  console.log(person)
+ document.write(person.firstName)
+ */
+//  puzzle();
+//  function puzzle(){
+//   console.log("testing Purpose");
+//  }
+
+// function razzle(lawyer="Billy", target ="'em"){
+//   console.log(`${lawyer} razzle-dazzles ${target}!`);
+// }
+// razzle();
+// razzle("Methuselah", "T'Challah");
+
+//  (function() { //it's wraping
+//   console.log("Yet more razzling");
+// })
+
+// ƒ () {
+//   console.log("Yet more razzling");
+// }
